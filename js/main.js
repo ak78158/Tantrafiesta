321c29bbdaac689fee03fa19ed517629fc5247db
Amit Kumar <ak78158@gmail.com>
2019-09-17 20:02:21 +0530

    added event pages and removed bugs

--- js/main.js
+++ js/main.js
@@ -126,7 +126,7 @@
 
 	  	$('#fh5co-hero .flexslider').flexslider({
 			animation: "fade",
-			slideshowSpeed: 3000,
+			slideshowSpeed: 5000,
 			directionNav: true,
 			start: function(){
 				setTimeout(function(){
@@ -156,28 +156,3 @@
 
 
 }());
-const year = new Date().getFullYear();
-const choosenDate = new Date(year, 8, 27).getTime();
-
-let countdown = setInterval(function() {
-
-  const today = new Date().getTime();
-
-  const diff = choosenDate - today;
-
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-  document.getElementById("countdown").innerHTML =
-    "<div class=\"days\"> \
-  <div class=\"c-number\">" + days + "</div>days</div> \
-<div class=\"hours\"> \
-  <div class=\"c-number\">" + hours + "</div>hours</div> \
-<div class=\"minutes\"> \
-  <div class=\"c-number\" >" + minutes + "</div>minutes</div> \
-<div class=\"seconds\"> \
-  <div class=\"c-number\">" + seconds + "</div>seconds</div> \
-</div>";
-
-}, 1000);
